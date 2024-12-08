9118702835540b1a6db6fcadce34b0e6bf4be2de
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2024-12-08 20:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+
+	let ar = [];
+
+	for (let index = 0; index < 26; index++) {
+		ar[i] = 0;
+	}
+
+	for (let index = 0; index < str.length; index++) {
+
+		let val = str[i].charCodeAt(0);
+
+		ar[val-97]++;
+	}
+
+	for (let index = 0; index < bound; index++) {
+
+		let val = str[i].charCodeAt(0);
+
+		if(ar[val] == 1) console.log(str[i])
+	}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
